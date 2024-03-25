c399ae68ec4876bc0dfe83f76c4681e4d9113934
marcos guilherme <guithomazs@gmail.com>
2024-03-25 07:03:53 -0500

    Starting project
    Funcionando extrair os dados de uma lista de arquivos em xml e mostrar os valores dos produtos, no entanto está tudo criado na função de rodar o spring, necessitando ainda criar e organizar as camadas para essa função, a ser feito após uma ideação do fluxo e modelos do banco

---
+++ back-end/src/main/java/com/review/models/Produto.java
@@ -0,0 +1,18 @@
+package com.review.models;
+
+public class Produto {
+    public String cProd;
+    public String cEAN;
+    public String xProd;
+    public String NCM;
+    public String CFOP;
+    public String uCom;
+    public Double qCom;
+    public Double vUnCom;
+    public Double vProd;
+    public String cEANTrib;
+    public String uTrib;
+    public Double qTrib;
+    public Double vUnTrib;
+    public String indTot;
+}
