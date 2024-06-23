117f4282ff0d743e0af702509f013da1d4753160
Mat3us3003 <mateusalmeida301@gmail.com>
2024-06-23 14:12:21 -0300

    Update app.routes.ts

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -2,11 +2,12 @@ import { Routes } from '@angular/router';
 import { InicioComponent } from './pages/inicio/inicio.component';
 import { IcmsHomeComponent } from './pages/icms/icms-home/icms-home.component';
 import { TesteComponent } from './pages/teste/teste.component';
+import { NotasProcessadasComponent } from './pages/notas-processadas/notas-processadas.component';
 
 export const routes: Routes = [
 
   { path: '',component: InicioComponent},
   { path: "icms", component: IcmsHomeComponent},
   { path: 'teste', component: TesteComponent},
-
+  { path: 'notas-processadas', component: NotasProcessadasComponent},
 ];
