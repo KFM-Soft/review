07303d01aca0539802a4a6386f3362aefd402a64
Felix-Vidal <joaofelixvidal@gmail.com>
2024-06-23 17:08:38 -0500

    :recycle: refactor: estilizando card empresa

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { InicioComponent } from './pages/inicio/inicio.component';
 import { IcmsHomeComponent } from './pages/icms/icms-home/icms-home.component';
 import { TesteComponent } from './pages/teste/teste.component';
 import { NotasProcessadasComponent } from './pages/icms/notas-processadas/notas-processadas.component';
+import {  NotasCarregarComponent } from './pages/icms/notas-carregar/notas-carregar.component'
 
 export const routes: Routes = [
 
   { path: '',component: InicioComponent},
-  { path: "icms", component: IcmsHomeComponent},
+  { path: 'icms', component: IcmsHomeComponent},
   { path: 'notas-processadas', component: NotasProcessadasComponent},
+  { path: 'notas-carregar', component: NotasCarregarComponent},
   { path: 'teste', component: TesteComponent},
 
 ];
