265a41eee103cba0160d29bef2d62c5fc7bda9e1
Felix-Vidal <joaofelixvidal@gmail.com>
2024-06-23 15:01:10 -0500

    :ok_hand: style: reorganizando pastas nostas-processadas para icms

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -2,12 +2,13 @@ import { Routes } from '@angular/router';
 import { InicioComponent } from './pages/inicio/inicio.component';
 import { IcmsHomeComponent } from './pages/icms/icms-home/icms-home.component';
 import { TesteComponent } from './pages/teste/teste.component';
-import { NotasProcessadasComponent } from './pages/notas-processadas/notas-processadas.component';
+import { NotasProcessadasComponent } from './pages/icms/notas-processadas/notas-processadas.component';
 
 export const routes: Routes = [
 
   { path: '',component: InicioComponent},
   { path: "icms", component: IcmsHomeComponent},
-  { path: 'teste', component: TesteComponent},
   { path: 'notas-processadas', component: NotasProcessadasComponent},
+  { path: 'teste', component: TesteComponent},
+
 ];
