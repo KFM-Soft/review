36e5b107dca30720eb177cf7b5748aa90fc76874
Felix-Vidal <joaofelixvidal@gmail.com>
2024-07-02 16:10:07 -0500

    empresa-form

--- front-end/src/app/pages/icms/empresa-form/empresa-form.component.spec.ts
+++ front-end/src/app/pages/icms/empresa-form/empresa-form.component.spec.ts
@@ -1,23 +1,22 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { fakeAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 import { EmpresaFormComponent } from './empresa-form.component';
 
 describe('EmpresaFormComponent', () => {
   let component: EmpresaFormComponent;
   let fixture: ComponentFixture<EmpresaFormComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [EmpresaFormComponent]
+  beforeEach(fakeAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ EmpresaFormComponent ]
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(EmpresaFormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+  }));
 
-  it('should create', () => {
+  it('should compile', () => {
     expect(component).toBeTruthy();
   });
 });
--- front-end/src/app/pages/icms/empresa-form/empresa-form.component.ts
+++ front-end/src/app/pages/icms/empresa-form/empresa-form.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
-
+import { FormControl, FormGroup, NonNullableFormBuilder, Validators, FormsModule } from '@angular/forms';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
 @Component({
   selector: 'app-empresa-form',
   standalone: true,
-  imports: [],
   templateUrl: './empresa-form.component.html',
-  styleUrl: './empresa-form.component.scss'
+  imports: [NzFormModule, NzInputModule, FormsModule]
 })
 export class EmpresaFormComponent {
+  validateForm: FormGroup<{
+    userName: FormControl<string>;
+    password: FormControl<string>;
+    remember: FormControl<boolean>;
+  }> = this.fb.group({
+    userName: ['', [Validators.required]],
+    password: ['', [Validators.required]],
+    remember: [true]
+  });
 
+  submitForm(): void {
+    console.log('submit', this.validateForm.value);
+  }
+
+  constructor(private fb: NonNullableFormBuilder) {}
 }
