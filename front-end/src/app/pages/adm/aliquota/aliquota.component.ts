25fb2e954015e99192c88e0c5c354b83e453b7cb
marcos guilherme <guithomazs@gmail.com>
2024-07-10 20:50:28 -0500

    teste

--- front-end/src/app/pages/adm/aliquota/aliquota.component.ts
+++ front-end/src/app/pages/adm/aliquota/aliquota.component.ts
@@ -1,12 +1,105 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterOutlet, RouterLink } from '@angular/router';
+import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzFlexModule } from 'ng-zorro-antd/flex';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzPaginationModule } from 'ng-zorro-antd/pagination';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzTableModule } from 'ng-zorro-antd/table';
 
 @Component({
   selector: 'app-aliquota',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    NzIconModule,
+    NzLayoutModule,
+    NzMenuModule,
+    NzBreadCrumbModule,
+    NzCardModule,
+    NzFlexModule,
+    NzTableModule,
+    NzButtonModule,
+    NzPaginationModule,
+    NzInputModule,
+    NzSelectModule,
+    FormsModule,
+    RouterLink,
+  ],
   templateUrl: './aliquota.component.html',
   styleUrl: './aliquota.component.scss'
 })
 export class AliquotaComponent {
+  empresas: Empresas[] = [
+    { nome: 'Tech Innovators Ltd', data_expirar: '15/09/2024', button: true, tag: 'tecnologia' },
+    { nome: 'Green Energy Corp', data_expirar: '27/04/2024', button: true, tag: 'agricola'  },
+    { nome: 'Future Solutions LLC', data_expirar: '11/11/2024', button: true, tag: 'tecnologia'  },
+    { nome: 'Apex Industries', data_expirar: '03/12/2024', button: true, tag: 'tecnologia'  },
+    { nome: 'NextGen Enterprises', data_expirar: '19/01/2025', button: true, tag: 'tecnologia'  },
+    { nome: 'Global Ventures Inc', data_expirar: '05/07/2024', button: true, tag: 'agricola'  },
+    { nome: 'Prime Logistics', data_expirar: '22/08/2024', button: true, tag: 'mercado'  },
+    { nome: 'EcoWorld Co', data_expirar: '14/10/2024', button: true, tag: 'agricola'  },
+    { nome: 'BlueSky Holdings', data_expirar: '29/06/2024', button: true, tag: 'tecnologia'  },
+    { nome: 'Visionary Tech Ltd', data_expirar: '21/03/2024', button: true, tag: 'tecnologia'  },
+    { nome: 'Smart Solutions Group', data_expirar: '06/02/2024', button: true, tag: 'tecnologia'  },
+    { nome: 'Infinity Networks', data_expirar: '18/05/2024', button: true, tag: 'mercado' },
+    { nome: 'Licença de EPP Disponível', data_expirar: '05/11/2024', button: false, tag: 'registrar' },
+    { nome: 'Licença de EPP Disponível', data_expirar: '05/11/2024', button: false, tag: 'registrar' },
+    { nome: 'Licença de EPP Disponível', data_expirar: '05/11/2024', button: false, tag: 'registrar' },
+  ];
 
+  registros: Empresas[] = [];
+  total = this.empresas.length;
+  paginaTamanho = 5;
+  paginaIndex = 1;
+  termoBusca: string = '';
+  selectTag: string = '';
+
+  ngOnInit(): void {
+    this.atualizarTabela();
+  }
+
+  atualizarTabela(): void {
+    let filtro = this.empresas;
+
+    if (this.termoBusca) {
+      filtro = filtro.filter(empresa => 
+        empresa.nome.toLowerCase().includes(this.termoBusca.toLowerCase())
+      );
+    }
+
+    if (this.selectTag) {
+      filtro = filtro.filter(empresa => empresa.tag === this.selectTag);
+    }
+
+    this.total = filtro.length;
+    const startIndex = (this.paginaIndex - 1) * this.paginaTamanho;
+    const endIndex = startIndex + this.paginaTamanho;
+    this.registros = filtro.slice(startIndex, endIndex);
+  }
+
+  atulizarPagina(paginaindex: number): void {
+    this.paginaIndex = paginaindex;
+    this.atualizarTabela();
+  }
+
+  tamanhoPagina(paginaTamanho: number): void {
+    this.paginaTamanho = paginaTamanho;
+    this.atualizarTabela();
+  }
+}
+
+interface Empresas {
+  nome: string;
+  data_expirar: string;
+  button: boolean;
+  tag: string;
 }
